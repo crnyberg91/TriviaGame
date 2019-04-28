8c0425239606da86bd952c53fcb2fdd9edde1c67
Chris Nyberg <chrisn83@gmail.com>
2019-04-28 17:53:51 -0400

    added answered function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
-window.onload = () => {
-    $('#start').on('click', timer);
-
-
+window.onload = function() {
+    $('#start').on('click', timer)
+    
 }
 
 let correct = false;
@@ -9,6 +8,7 @@ let timeLeft = 10;
 var timerRunning = false;
 var nextQuestion = false;
 var intervalId;
+var nextQTime;
 
 const question = {
     q: 'is this working',
@@ -24,14 +24,19 @@ const question = {
 
 function timer() {
     $('#start').remove()
-    clearInterval(intervalId);
-    intervalId = setInterval(countdown, 1000);
-    displayQuestion();
-    timerRunning = true;
-    if(!timerRunning) {
-        clearInterval(intervalId);
-        //answered function
+    $('#time-line').html(`<p>30 seconds left</p>`);
+    
+    if (!timerRunning) {
+        timerRunning = true;
+        intervalId = setInterval(countdown, 1000);
+        displayQuestion();
     }
+}
+
+function stop() {
+    clearInterval(intervalId);
+    timerRunning = false;
+    //answered function
 }
 
 function countdown() {
@@ -39,22 +44,30 @@ function countdown() {
     $('#time-line').html(`<p>${timeLeft} seconds left</p>`);
 
     if (timeLeft === 0) {
-        timerRunning = false;
-        intervalId = clearInterval(timeLeft);
+        stop();
+        console.log('times up');
+        answered();
     }
+ 
+    // if (timeLeft === 0) {
+    //     timerRunning = false;
+    // }
 }
 
-function displayQuestion () {
+function displayQuestion() {
     $('#questions-box').append(`<h2>${question.q}<h2>`);
-    $('#questions-box').append(`<h4>A: ${question.a}</h4>`);
-    $('#questions-box').append(`<h4>A: ${question.b}</h4>`);
-    $('#questions-box').append(`<h4>A: ${question.c}</h4>`);
-    $('#questions-box').append(`<h4>A: ${question.d}</h4>`);
+    $('#questions-box').append(`<h4><a>A: ${question.a}</a></h4>`);
+    $('#questions-box').append(`<h4><a>A: ${question.b}</a></h4>`);
+    $('#questions-box').append(`<h4><a>A: ${question.c}</a></h4>`);
+    $('#questions-box').append(`<h4><a>A: ${question.d}</a></h4>`);
 }
 
 function answered() {
-    //start new timer for next question
-    //if correct wincount++ else wrongCount++
+    $('#questions-box').empty()
+    nextQTime = setInterval(timer, 1000 * 10);
+    $('#questions-box').append('it works');
+    console.log(nextQTime)
+    timeLeft = 10;
     //when next question timer === 0, change timeRunning to true
     //display next question
 }
