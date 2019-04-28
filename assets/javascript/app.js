101f59bf5c7ec572c64ceeac447878cb5011619e
Chris Nyberg <chrisn83@gmail.com>
2019-04-28 11:39:41 -0400

    start and timer functions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,61 @@
-let correct=false;
+window.onload = () => {
+    $('#start').on('click', timer);
 
+
+}
+
+let correct = false;
+let timeLeft = 10;
+var timerRunning = false;
+var nextQuestion = false;
+var intervalId;
 
 const question = {
+    q: 'is this working',
     a: 'a',
     b: 'b',
     c: 'c',
     d: 'd',
     gif: 'giphyurl',
-    correct(){
-        
+    correct() {
+
     }
 }
 
-$('#start').on('click',function(){
-    event.preventDefault();
+function timer() {
+    $('#start').remove()
+    clearInterval(intervalId);
+    intervalId = setInterval(countdown, 1000);
+    displayQuestion();
+    timerRunning = true;
+    if(!timerRunning) {
+        clearInterval(intervalId);
+        //answered function
+    }
+}
 
-        
+function countdown() {
+    timeLeft--;
+    $('#time-line').html(`<p>${timeLeft} seconds left</p>`);
 
+    if (timeLeft === 0) {
+        timerRunning = false;
+        intervalId = clearInterval(timeLeft);
+    }
+}
 
+function displayQuestion () {
+    $('#questions-box').append(`<h2>${question.q}<h2>`);
+    $('#questions-box').append(`<h4>A: ${question.a}</h4>`);
+    $('#questions-box').append(`<h4>A: ${question.b}</h4>`);
+    $('#questions-box').append(`<h4>A: ${question.c}</h4>`);
+    $('#questions-box').append(`<h4>A: ${question.d}</h4>`);
+}
 
+function answered() {
+    //start new timer for next question
+    //if correct wincount++ else wrongCount++
+    //when next question timer === 0, change timeRunning to true
+    //display next question
+}
 
-})
